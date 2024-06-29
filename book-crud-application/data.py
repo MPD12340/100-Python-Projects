d1829d8ce240a459fa49044fceb1405d86741214
mahendra dhamala <rijalmpd1234@gmail.com>
2024-06-29 15:18:17 +0545

    fastapi book-crud commit

---
+++ book-crud-application/data.py
@@ -0,0 +1,37 @@
+books = [
+  {
+    "id": "9781593275846",
+    "title": "Eloquent JavaScript, 3rd Edition",
+    "pageCount": 472,
+    "price": "$39.95",
+    "publication": "No Starch Press"
+  },
+  {
+    "id": "9780132350884",
+    "title": "Clean Code: A Handbook of Agile Software Craftsmanship",
+    "pageCount": 464,
+    "price": "$49.99",
+    "publication": "Prentice Hall"
+  },
+  {
+    "id": "9780596009205",
+    "title": "Head First Design Patterns",
+    "pageCount": 694,
+    "price": "$59.99",
+    "publication": "O'Reilly Media"
+  },
+  {
+    "id": "9780132350884",
+    "title": "Clean Code: A Handbook of Agile Software Craftsmanship",
+    "pageCount": 464,
+    "price": "$49.99",
+    "publication": "Prentice Hall"
+  },
+  {
+    "id": "9781492041259",
+    "title": "Python Crash Course, 2nd Edition",
+    "pageCount": 544,
+    "price": "$39.99",
+    "publication": "No Starch Press"
+  }
+]
